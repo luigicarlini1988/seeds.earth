97f7401d90470599e7617cf6902344878e271f5d
Luigi Carlini <luigicarlini88@gmail.com>
2021-03-23 13:23:44 +0530

    Commit changes on menu text from Atom

--- incl/menu.php
+++ incl/menu.php
@@ -34,7 +34,7 @@
               <p class="menu-tit">Who is behind SEEDS?</p>
               <p class="menu-text">
                 <span class="opct">
-              SEEDS is owned by no-one and designed for everyone. For it to be a truly decentralised financial and political system, the organisation behind it needs to be decentralised. For this reason SEEDS is being built by Hypha. Hypha DHO is a global, transparent, open-organisation that is owned by all who contribute and open to all! Anyone, yes that includes you, can contribute to this and get paid for doing so.
+              SEEDS is owned by no-one and designed for everyone. For it to be a truly decentralised financial and political system, the organisation behind it needs to be decentralised. For this reason SEEDS is co-created by Hypha together with the Seeds Community. Hypha DHO is a global, transparent, open-organisation that is owned by all who contribute and open to all! Anyone, yes that includes you, can contribute to this and get paid for doing so.
               <br/><br/>To see the </span><a href="https://dho.hypha.earth/#/members" target="_blank">members behind SEEDS</a><span class="opct">, how this all works and to co-create with us, come explore the </span><a href="https://dho.hypha.earth" target="_blank">Hypha.DHO.</a><span class="opct"> Hypha is a radically transparent organism, so for that reason anyone can see our </span><a href="https://docs.google.com/spreadsheets/d/10URjhJZBaf5YbPBsMCBm9anzJfaUZLKrDinwc8BQK80/edit#gid=182812459" target="_blank">Financial Dashboard.</a> </p>
 
             </div>
