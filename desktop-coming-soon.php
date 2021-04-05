42dbe5e71a0d2dd6298514cd1b9f9560fb3ee781
Luigi Carlini <luigicarlini88@gmail.com>
2021-04-05 12:05:22 +0530

    google analytics script

--- desktop-coming-soon.php
+++ desktop-coming-soon.php
@@ -26,7 +26,15 @@
 
     <!--  Twitter -->
 
-    <!--  Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L2XGY9YP0R"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-L2XGY9YP0R');
+    </script>
 
 
     <!-- CSS -->
